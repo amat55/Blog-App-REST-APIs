c13c784c5d2f750e9387bcee482384e7c8f44c73
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2023-10-07 16:40:01 +0200

    Created JPA Entity - Post Entity

---
+++ src/main/java/com/ahmetsenocak/blogapp/entity/Post.java
@@ -0,0 +1,26 @@
+package com.ahmetsenocak.blogapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = "title")})
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "content")
+    private String content;
+}
