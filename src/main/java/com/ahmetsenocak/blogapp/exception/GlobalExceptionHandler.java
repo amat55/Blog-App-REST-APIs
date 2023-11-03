ea39b6df77670c51e000989a297f0d7677b617eb
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2023-11-03 15:10:07 +0100

    Created Global Exception Handling

--- src/main/java/com/ahmetsenocak/blogapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ahmetsenocak/blogapp/exception/GlobalExceptionHandler.java
@@ -27,4 +27,12 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
+    // Global exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                               WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
