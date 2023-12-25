9592f5077cb1fdd921e045db6cd571306c750dec
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2023-12-25 18:49:03 +0100

    Created API Information

--- src/main/java/com/ahmetsenocak/blogapp/BlogAppApplication.java
+++ src/main/java/com/ahmetsenocak/blogapp/BlogAppApplication.java
@@ -1,11 +1,36 @@
 package com.ahmetsenocak.blogapp;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Spring Boot Blog App Rest APIs",
+                description = "Spring Boot Blog App Rest APIs Documentation",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Ahmet",
+                        email = "ahmetsaidsenocak312@gmail.com",
+                        url = "https://www.linkedin.com/in/ahmet-said-senocak-a46795196/"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Spring Boot Blog App Documentation",
+                url = "https://github.com/amat55/Blog-App-REST-APIs"
+        )
+)
 public class BlogAppApplication {
 
     @Bean
