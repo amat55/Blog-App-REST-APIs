7f6099e26d27344faf0fe6846b655c2a1feb6348
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2023-12-26 01:07:48 +0100

    Customized Swagger Models Documentation

--- src/main/java/com/ahmetsenocak/blogapp/payload/PostDTO.java
+++ src/main/java/com/ahmetsenocak/blogapp/payload/PostDTO.java
@@ -1,5 +1,6 @@
 package com.ahmetsenocak.blogapp.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -7,20 +8,27 @@ import lombok.Data;
 import java.util.Set;
 
 @Data
+@Schema(
+        description = "PostDTO model information"
+)
 public class PostDTO {
     private Long id;
 
+    @Schema(description = "Blog Post Title")
     @NotEmpty
     @Size(min = 2, message = "title must be at least 2 character")
     private String title;
 
+    @Schema(description = "Blog Post description")
     @NotEmpty
     @Size(min = 10, message = "Title must be at least 10 character")
     private String description;
 
+    @Schema(description = "Blog Post content")
     @NotEmpty
     private String content;
     private Set<CommentDTO> comments;
 
+    @Schema(description = "Blog Post category")
     private Long categoryId;
 }
