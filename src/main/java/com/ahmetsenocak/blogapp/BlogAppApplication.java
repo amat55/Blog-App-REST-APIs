46c5538612f5fac264efacdca1f5d8d701737f88
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2023-12-29 01:31:57 +0100

    Created - Insert Metadata in Tables

--- src/main/java/com/ahmetsenocak/blogapp/BlogAppApplication.java
+++ src/main/java/com/ahmetsenocak/blogapp/BlogAppApplication.java
@@ -1,11 +1,15 @@
 package com.ahmetsenocak.blogapp;
 
+import com.ahmetsenocak.blogapp.entity.Role;
+import com.ahmetsenocak.blogapp.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -31,10 +35,10 @@ import org.springframework.context.annotation.Bean;
                 url = "https://github.com/amat55/Blog-App-REST-APIs"
         )
 )
-public class BlogAppApplication {
+public class BlogAppApplication implements CommandLineRunner {
 
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
@@ -42,4 +46,17 @@ public class BlogAppApplication {
         SpringApplication.run(BlogAppApplication.class, args);
     }
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
+    }
 }
